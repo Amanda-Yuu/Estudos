f33bd70ab06c2fff17f5db11cce65ad5a91a09f2
amanda silveira <amanda.silveira0w@gmail.com>
2025-04-29 13:24:32 -0300

    Atividade 2 - Curso POO

--- python/POO_Atividade_PY/modelos/restaurante.py
+++ python/POO_Atividade_PY/modelos/restaurante.py
@@ -1,14 +1,25 @@
 class Restaurante:
+    restaurantes = []
     #self é uma propriedade dentro de uma classe
     def __init__(self, nome, categoria):
         self.nome = nome
         self.categoria = categoria
-        self.ativo = False
+        self._ativo = False
+        Restaurante.restaurantes.append(self)
 
         #__str__ é usado para mostrar as info em sting 
     def __str__(self): 
         return f'{self.nome} | {self.categoria}'
         
+    def listar_restaurantes():
+        print(f'{'Nome do Restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'.ljust(25)}')
+        for restaurante in Restaurante.restaurantes:
+            print(f'{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')
+
+    @property
+    def ativo(self):
+        return 'Ativo' if self._ativo else 'Desativo'
+
 restaurante_praca = Restaurante('Praça', 'Gourmet')
 restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
 
@@ -16,3 +27,5 @@ restaurantes = [restaurante_praca, restaurante_pizza]
 
 print(restaurante_praca)
 print(restaurante_pizza)
+
+Restaurante.listar_restaurantes()
