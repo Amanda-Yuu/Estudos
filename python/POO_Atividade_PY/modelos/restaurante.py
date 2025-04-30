90434b76d16cbd6ead84815ef03c6f42d9b4c27f
amanda silveira <amanda.silveira0w@gmail.com>
2025-04-30 11:45:08 -0300

    Atividade 3 - curso POO

--- python/POO_Atividade_PY/modelos/restaurante.py
+++ python/POO_Atividade_PY/modelos/restaurante.py
@@ -1,31 +1,42 @@
+from modelos.avaliacao import Avaliacao
+
 class Restaurante:
     restaurantes = []
     #self é uma propriedade dentro de uma classe
     def __init__(self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
+        self._nome = nome.title()
+        self._categoria = categoria.upper()
         self._ativo = False
+        self._avaliacao = []
         Restaurante.restaurantes.append(self)
 
         #__str__ é usado para mostrar as info em sting 
     def __str__(self): 
-        return f'{self.nome} | {self.categoria}'
-        
-    def listar_restaurantes():
-        print(f'{'Nome do Restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'.ljust(25)}')
-        for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')
+        return f'{self._nome} | {self._categoria}'
+
+    @classmethod
+    def listar_restaurantes(cls):
+        print(f'{'Nome do Restaurante'.ljust(25)} | {'Categoria'.ljust(25)}  | {'Avaliação'.ljust(25)} | {'Status'.ljust(25)}')
+        for restaurante in cls.restaurantes:
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)}  | {str(restaurante.media_avaliacoes).ljust(25)} | {restaurante.ativo}')
 
     @property
     def ativo(self):
-        return 'Ativo' if self._ativo else 'Desativo'
+        return '⌧' if self._ativo else '☐'
 
-restaurante_praca = Restaurante('Praça', 'Gourmet')
-restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
+    def alternar_estado(self):
+        self._ativo = not self._ativo
 
-restaurantes = [restaurante_praca, restaurante_pizza]
+    def receber_avaliacao(self, cliente, nota):
+        if 0 < nota <= 5:
+            avaliacao = Avaliacao(cliente, nota)
+            self._avaliacao.append(avaliacao)
 
-print(restaurante_praca)
-print(restaurante_pizza)
-
-Restaurante.listar_restaurantes()
+    @property
+    def media_avaliacoes(self):
+        if not self._avaliacao:
+            return 'Sem avaliação'
+        soma_das_notas = sum(avaliacao._nota for avaliacao in self._avaliacao)
+        quantidade_de_notas = len(self._avaliacao)
+        media = round(soma_das_notas / quantidade_de_notas, 1)
+        return media
