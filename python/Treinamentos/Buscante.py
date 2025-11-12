5925dfdc851fbb36b5bbb7b97e9301c0e197a6be
amanda silveira <amanda.silveira0w@gmail.com>
2025-11-12 18:29:13 -0300

    Desafio 2 while E for

---
+++ python/Treinamentos/Buscante.py
@@ -0,0 +1,11 @@
+# contador = 0
+
+# while contador <5:
+#     print("Bem-vindo buscante!")
+#     contador +=1
+
+#----------------------------------
+
+#resposta do instrutor:
+for i in range (5):
+    print("Bem-vindo buscante!")
---
+++ python/Treinamentos/Loop_infinito.py
@@ -0,0 +1,6 @@
+contador = 0
+
+while contador < 10:
+    print("Processando dados...")
+    contador += 1 # O contador sempre estava no zero, nada somava, portanto nunca atingiria a condição que parasse a contagem, levando ao loop infinito.
+
